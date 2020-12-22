ae5221eccf5ea88f4f689c4d2b3dd329f099b560
Oren Kravetz <okravetz@outlook.com>
2020-12-22 14:15:13 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     var myArr = JSON.parse(this.responseText);
-    document.getElementById("demo").innerHTML = myArr[he][0];
+    document.getElementById("demo").innerHTML = myArr[0].he;
   }
 };
 xmlhttp.open("GET", "https://www.sefaria.org/api/texts/Psalms_150", true);
