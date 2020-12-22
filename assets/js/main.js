3140b00be000eb93d0b009f6f360b460f170ec19
Oren Kravetz <okravetz@outlook.com>
2020-12-22 14:09:16 -0500

    fixing js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,8 @@ var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     var myArr = JSON.parse(this.responseText);
-    document.getElementById("demo").innerHTML = myArr[0];
+    document.getElementById("demo").innerHTML = myArr[he][0];
   }
 };
-xmlhttp.open("GET", "https://www.sefaria.org/api/texts/Psalms%20150", true);
+xmlhttp.open("GET", "https://www.sefaria.org/api/texts/Psalms_150", true);
 xmlhttp.send();
